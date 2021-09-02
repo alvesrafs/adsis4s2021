590f3bcd99b2767e81353eecec3e1695d48fd5f0
Rafael <rafael_mello_13@hotmail.com>
2021-09-01 21:37:47 -0300

    Atualizando

---
+++ src/main/java/br/unicesumar/adsis4s2021/meu/pet/MeuPet.java
@@ -0,0 +1,26 @@
+package br.unicesumar.adsis4s2021.meu.pet;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class MeuPet {
+	@Id
+	private String id;
+	private String nome;
+	
+	public MeuPet() {
+	}
+	
+	public MeuPet(String id, String nome) {
+		this.id = id;
+		this.nome = nome;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	public String getNome() {
+		return nome;
+	}
+}
